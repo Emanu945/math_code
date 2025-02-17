114040988239b468d0a3b97ac0fc2548c2dd38c3
Emanu945 <emanuelbrito945@gmail.com>
2025-02-16 22:23:01 -0300

    Initial commit

---
+++ scripts/panel.gd
@@ -0,0 +1,18 @@
+extends Panel
+
+func _ready() -> void:
+	var gerarX = 0
+	var gerarY = 0
+	
+	const min = 0
+	const max = 10
+	
+	gerarX = randi_range(min,max)
+	gerarY = randi_range(min,max)
+
+	print("Quanto é ",str(gerarX),"+",str(gerarY)," ?")
+	get_node("LblText").set_text("Quanto é "+str(gerarX)+"+"+str(gerarY)+" ?")
+	pass 
+	
+func _process(_delta: float) -> void:
+	pass
